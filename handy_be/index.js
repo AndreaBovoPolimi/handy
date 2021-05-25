eec1e8f351e913ef96e37d9003d6d106f328ffc2
Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
2021-05-25 13:47:18 +0200

    backend

--- handy_be/index.js
+++ handy_be/index.js
@@ -1,113 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const expressSwagger = require('express-swagger-generator')(app);
 
-const url = 'mongodb://localhost:27017';
-const { MongoClient } = require('mongodb');
+const apiEvents = require('./events-api');
 
 app.use(express.json());
 
-let options = {
-  swaggerDefinition: {
-    info: {
-      description: 'This is a sample server',
-      title: 'Swagger',
-      version: '3.0.3',
-    },
-    host: 'localhost:3000',
-    basePath: '/v1',
-    produces: ['application/json', 'application/xml'],
-    schemes: ['http', 'https'],
-    securityDefinitions: {
-      JWT: {
-        type: 'apiKey',
-        in: 'header',
-        name: 'Authorization',
-        description: 'apiKey',
-      },
-    },
-  },
-  basedir: __dirname, //app absolute path
-  files: ['./routes/**/*.js'], //Path to the API handle folder
-};
-expressSwagger(options);
+// get all upcoming events
+app.get('/events', apiEvents.getAll);
 
-app.get('/events', (req, res) => {
-  dbConnect((db) => {
-    const events = db
-      .collection('events')
-      .find(
-        req.query.participantUser ? { participantUsers: { $elemMatch: { userId: req.query.participantUser } } } : {}
-      )
-      .project({ participantUsers: 0 })
-      .toArray()
-      .then((events) => {
-        console.log(events);
-        res.send(events);
-      });
-  });
-});
+// create a new event
+app.post('/events', apiEvents.create);
 
-app.post('/events', (req, res) => {
-  const event = req.body;
+// user subscribe to event
+app.put('/events/:id/subscribe', apiEvents.subscribe);
 
-  dbConnect((db) => {
-    db.collection('events').insertOne(event);
-    res.json({
-      success: true,
-      insertedEvent: req.body,
-    });
-  });
-});
-
-app.put('/events/:id/subscribe', (req, res) => {
-  dbConnect((db) => {
-    console.log(req.params.id);
-    db.collection('events').updateOne(
-      { _id: req.params.id },
-      {
-        $push: {
-          participantUsers: {
-            userId: req.body.userId,
-            registeredAt: (Date.now() / 1000) | 0,
-            payment: null,
-          },
-        },
-      }
-    );
-    res.json({
-      success: true,
-    });
-  });
-});
-
-app.put('/events/:id/unsubscribe', (req, res) => {
-  dbConnect((db) => {
-    db.collection('events').updateOne(
-      { _id: req.params.id },
-      {
-        $pull: {
-          participantUsers: {
-            userId: req.body.userId,
-          },
-        },
-      }
-    );
-    res.json({
-      success: true,
-    });
-  });
-});
+// user unsubscribe to event
+app.put('/events/:id/unsubscribe', apiEvents.unsubscribe);
 
 app.listen(port, () => console.log(`Handy backend listening at http://localhost:${port}`));
-
-function dbConnect(query) {
-  MongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
-    if (err) return console.error(err);
-    var dbo = db.db('handy_events');
-    query(dbo, () => {
-      db.close();
-    });
-  });
-}
